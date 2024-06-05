f7566dfd476df3bf5a967a69f1c7ecc14f700888
Paolo <kos.paolo@gmail.com>
2024-06-05 22:05:34 +0200

    Home page styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { Service } from '../models/service.model';
+import { Customer } from '../models/customer.model';
+import { Offer } from '../models/offer.model';
+import { Invoice } from '../models/invoice.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  services: Service[] = [];
+  customers: Customer[] = [];
+  offers: Offer[] = [];
+  invoices: Invoice[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.fetchServices();
+    this.fetchCustomers();
+    this.fetchOffers();
+    this.fetchInvoices();
+  }
+
+  fetchServices(): void {
+    this.apiService.fetchServices().subscribe(
+      services => {
+        this.services = services;
+      },
+      error => {
+        console.error('Error fetching services', error);
+      }
+    );
+  }
+
+  fetchCustomers(): void {
+    this.apiService.fetchCustomers().subscribe(
+      customers => {
+        this.customers = customers;
+      },
+      error => {
+        console.error('Error fetching customers', error);
+      }
+    );
+  }
+
+  fetchOffers(): void {
+    this.apiService.fetchOffers().subscribe(
+      offers => {
+        this.offers = offers;
+      },
+      error => {
+        console.error('Error fetching offers', error);
+      }
+    );
+  }
+
+  fetchInvoices(): void {
+    this.apiService.fetchInvoices().subscribe(
+      invoices => {
+        this.invoices = invoices;
+      },
+      error => {
+        console.error('Error fetching invoices', error);
+      }
+    );
+  }
 }
